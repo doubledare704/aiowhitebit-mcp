fbeedca96c867e06de987f354531fe5f9239c432
oleksiiovdiienko <doubledare704@gmail.com>
2025-04-19 12:44:26 +0300

    preparation for 1st release

--- setup.py
+++ setup.py
@@ -40,11 +40,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     extras_require={
         "dev": development_requires,
     },
